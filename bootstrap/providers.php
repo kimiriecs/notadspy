d1aaab4ed607ea6925e1c0a77d369f7cdbe2afb5
kimi dev <kimi@example.com>
2025-06-09 06:45:34 +0300

    refs v1.0.0 | feat: installed horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Modules\AdSpy\Provider\AdSpyServiceProvider::class,
     App\Modules\User\Provider\UserServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
 ];
